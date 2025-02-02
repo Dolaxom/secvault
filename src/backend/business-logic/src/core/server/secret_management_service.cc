df401362a421fcd187e057384139cb5ed1d7c91e
Nikolai Ryzhov <dolaxom4@gmail.com>
2025-02-02 18:17:46 +0300

    [BL][gRPC][Secrets Service][WIP] Adding test workflow with db

--- src/backend/business-logic/src/core/server/secret_management_service.cc
+++ src/backend/business-logic/src/core/server/secret_management_service.cc
@@ -1,29 +1,73 @@
 #include "secret_management_service.h"
 
+#include <common/crypto.h>
 #include <core/token.h>
 
 namespace sv::server {
 
 using namespace grpc;
 
+namespace rules {
+
+struct Database {
+  i32 poolSize;
+  postgres::ConnectionRules connRules;
+};
+
+}  // namespace rules
+
+SecretManagementService::SecretManagementService() {
+  // only for testing
+  // TODO remove it
+  postgres::ConnectionRules tempConnRules{
+      .host = "localhost",
+      .port = "5432",
+      .dbname = "sv_test",
+      .dbuser = "postgres",
+      .dbpass = "test_pass"};
+
+  rules::Database dbSetupRules;
+  dbSetupRules.poolSize = 8;
+  dbSetupRules.connRules = tempConnRules;
+
+  pgPool_ = std::make_shared<postgres::Pool>();
+  pgPool_->Build(dbSetupRules.poolSize, dbSetupRules.connRules);
+}
+
 Status SecretManagementService::WriteSecret(ServerContext* context, const secretmanagement::WriteSecretRequest* request, secretmanagement::WriteSecretResponse* response) {
-  auto password = request->password();
+  auto token = core::Token::CreateRandom().Dump();
+  auto secretEncrypted = sv::Crypto::Encrypt(request->secret(), request->password());
 
-  core::Token token;
-  token.Generate();
+  auto dbConnection = pgPool_->GetConnection();
+  if (InsertSecretDb(dbConnection, token, secretEncrypted) != Result::kOk) {
+    pgPool_->FreeConnection(dbConnection);
+    return Status{StatusCode::UNAVAILABLE, "Server error"};
+  }
+  pgPool_->FreeConnection(dbConnection);
 
-  // Запись в бд
+  response->set_first_token(token);
+  response->set_second_token(request->password());
 
-  response->set_first_token(token.Dump());
-  response->set_second_token(password);
-
-  return {};
+  return Status{};
 }
 
 Status SecretManagementService::ReadSecret(ServerContext* context, const secretmanagement::ReadSecretRequest* request, secretmanagement::ReadSecretResponse* response) {
   response->set_secret("Not implemented.");
 
   return {};
+}
+
+sv::Result SecretManagementService::InsertSecretDb(std::shared_ptr<postgres::Connection>& connection, const std::string& token, const std::string& secret) {
+  const char* query = "INSERT INTO secrets (id, message) VALUES ($1, $2);";
+  const char* values[] = {token.c_str(), secret.c_str()};
+  PGresult* result = PQexecParams(connection->GetRaw().get(), query, 2, nullptr, values, nullptr, nullptr, 0);
+  if (PQresultStatus(result) != PGRES_COMMAND_OK) {
+    return Result::kDbError;
+  }
+
+  PQclear(result);
+
+  return Result::kOk;
 }
 
 }  // namespace sv::server
--- src/backend/business-logic/src/core/server/secret_management_service.h
+++ src/backend/business-logic/src/core/server/secret_management_service.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <common/types.h>
+#include <database/pg_pool.h>
 #include <grpcpp/grpcpp.h>
 #include <secret_management.grpc.pb.h>
 
@@ -7,6 +9,8 @@ namespace sv::server {
 
 class SecretManagementService final : public secretmanagement::SecretService::Service {
  public:
+  SecretManagementService();
+
   grpc::Status WriteSecret(grpc::ServerContext* context,
                            const secretmanagement::WriteSecretRequest* request,
                            secretmanagement::WriteSecretResponse* response);
@@ -14,6 +18,14 @@ class SecretManagementService final : public secretmanagement::SecretService::Se
   grpc::Status ReadSecret(grpc::ServerContext* context,
                           const secretmanagement::ReadSecretRequest* request,
                           secretmanagement::ReadSecretResponse* response);
+
+ private:
+  std::shared_ptr<postgres::Pool> pgPool_;
+
+ private:
+  sv::Result InsertSecretDb(std::shared_ptr<postgres::Connection>& connection,
+                            const std::string& token,
+                            const std::string& secret);
 };
 
 }  // namespace sv::server
