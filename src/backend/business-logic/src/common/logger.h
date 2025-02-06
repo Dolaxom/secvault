d27cfe2a236ad9fb5a3516c7e2fa18ef1dacbdfe
Nikolai Ryzhov <dolaxom4@gmail.com>
2025-02-06 16:17:18 +0300

    [BL][Logger] Fixing Logger for files

--- src/backend/business-logic/src/common/logger.h
+++ src/backend/business-logic/src/common/logger.h
@@ -2,10 +2,10 @@
 
 #define SPDLOG_FMT_EXTERNAL
 #include <spdlog/async.h>
+#include <spdlog/async_logger.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
-#include <spdlog/async_logger.h>
 
 #include <filesystem>
 #include <memory>
@@ -47,9 +47,7 @@ class Logger {
         break;
     }
 
-    if (type == LogType::File || type == LogType::FileAndStdout) {
-      fileLogger_->log(lvl, message);
-    }
+    fileLogger_->log(lvl, message);
     if (type == LogType::FileAndStdout) {
       stdoutLogger_->log(lvl, message);
     }
@@ -59,15 +57,23 @@ class Logger {
   Logger(const std::filesystem::path& file) : file_(file) {
     spdlog::init_thread_pool(8192, 1);
 
-    fileLogger_ = spdlog::basic_logger_mt<spdlog::async_factory>("async_file_logger", file_.string());
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(file_.string(), true);
+    fileLogger_ = std::make_shared<spdlog::async_logger>(
+        "async_file_logger", file_sink, spdlog::thread_pool(),
+        spdlog::async_overflow_policy::block);
+
+    spdlog::register_logger(fileLogger_);
 
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     stdoutLogger_ = std::make_shared<spdlog::async_logger>(
         "async_console_logger", console_sink, spdlog::thread_pool(),
         spdlog::async_overflow_policy::block);
+
     spdlog::register_logger(stdoutLogger_);
 
     fileLogger_->set_pattern("%Y-%m-%d %H:%M:%S.%e [%l] %v");
+    fileLogger_->flush_on(spdlog::level::info);
+
     stdoutLogger_->set_pattern("%Y-%m-%d %H:%M:%S.%e [%l] %v");
   }
 
